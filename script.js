0857e76012652243dd8561f32ca9b37a02e1b733
Andy <andy_chan96@hotmail.com>
2022-03-06 15:26:39 +0800

    Added button and game announces winner at score 5

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 var myArray = ['Rock', 'Paper', 'Scissors'];
-
+let computerSelection;
 let computerScore = 0;
 let playerScore = 0;
+//let winner = '';
+let buttons = document.querySelectorAll('.button');
+
+const container = document.querySelector('#container');
+
+const myScore = document.createElement('div');
+myScore.classList.add('myScore');
+container.appendChild(myScore);
+
+const compScore = document.createElement('div');
+compScore.classList.add('compScore');
+container.appendChild(compScore);
+
+const winner = document.createElement('div');
+winner.classList.add('winner');
+container.appendChild(winner);
 
 function computerPlay() {
     const computerRandom = Math.floor(Math.random() * myArray.length);
-    return myArray[computerRandom];
+    return myArray[computerRandom].toUpperCase();
 }
 
-function singleRound() {
+buttons.forEach((test) => {
+    test.addEventListener('click', () => {
+        playerSelection = test.id.toUpperCase();
+        singleRound(playerSelection, computerSelection);
+    })
+})
 
-    const playerSelection = prompt('Rock, Paper or Scissors?').toUpperCase();
+function singleRound(playerSelection, computerSelection) {
 
-    if (['ROCK', 'PAPER', 'SCISSORS'].indexOf(playerSelection) < 0) {
-        alert('Wrong input!');
-        return;
-    }
-    const computerSelection = computerPlay().toUpperCase();
+    computerSelection = computerPlay();
 
     if (playerSelection === computerSelection) {
         const result = 'Draw! | Player: ' + playerScore + ' | Computer: ' + computerScore;
-        //console.log('Computer: ' + computerSelection);
-        //console.log('Player: ' + playerSelection);
-        return result;
     }
     else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') || (playerSelection === 'PAPER' && computerSelection === 'ROCK')) {
         playerScore += 1;
-        const result = 'You win! | Player: ' + playerScore + ' | Computer: ' + computerScore;
-        //console.log('Computer: ' + computerSelection);
-        //console.log('Player: ' + playerSelection);
-        return result;
-    }
+        myScore.textContent = 'Your score: ' + playerScore;
+        }
     else {
         computerScore += 1;
-        const result = 'You lose! | Player: ' + playerScore + ' | Computer: ' + computerScore;
-        //console.log('Computer: ' + computerSelection);
-        //console.log('Player: ' + playerSelection);
-        return result;
+        compScore.textContent = 'Computer score: ' + computerScore;
     }
+
+    scorecheck();
 }
 
-function game() {
+function scorecheck() {
 
-    for (let i=0; i<Infinity; i++) {
-        console.log(singleRound());
+            if (playerScore == 5) {
+                console.log('You won!');
+                winner.textContent = 'You won!'
+            }
 
-        if (playerScore == 5) {
-            console.log('You won!');
-            break;
-        }
-
-        else if (computerScore == 5) {
-            console.log('You lost!');
-            break;
-        }
-    }
+            else if (computerScore == 5) {
+                console.log('You lost!');
+                winner.textContent = 'You lost!'
+            }
 
 }
